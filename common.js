2113fec553a13e1a99efe0878858536284f576d3
Andrey Pogodin <pogodin@gmail.com>
2017-12-05 16:10:17 -0800

    added weekly

--- common.js
+++ common.js
@@ -3,3 +3,25 @@ var na = "--";
 var tickerLookup = [
     { BTC: "Bitcoin" }, { DASH: "Dash" }, { ETH: "Ethereum" }, { BCH: "Bitcoin Cash" }, { BTG: "Bitcoin Gold" }, { XRP: "Ripple" }
 ];
+
+function findTickerDescription(ticker) {
+    let retVal = null;
+    for (var i = 0; i < tickerLookup.length; i++) {
+        var nextPair = tickerLookup[i];
+        if (nextPair[ticker] != null) {
+            retVal = nextPair[ticker];
+            break;
+        }
+    }
+    return retVal;
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
